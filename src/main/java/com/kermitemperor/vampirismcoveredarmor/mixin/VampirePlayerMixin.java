44b766a2f7e7f685d07ee93bd212d1587afab668
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-10-12 16:27:53 +0200

    Shortened Code before upload

--- src/main/java/com/kermitemperor/vampirismcoveredarmor/mixin/VampirePlayerMixin.java
+++ src/main/java/com/kermitemperor/vampirismcoveredarmor/mixin/VampirePlayerMixin.java
@@ -2,11 +2,11 @@ package com.kermitemperor.vampirismcoveredarmor.mixin;
 
 
 
-import com.kermitemperor.vampirismcoveredarmor.VampirismCoveredArmor;
 import com.kermitemperor.vampirismcoveredarmor.config.VCACommonConfigs;
 import de.teamlapen.vampirism.api.entity.player.vampire.IVampirePlayer;
 import de.teamlapen.vampirism.entity.player.FactionBasePlayer;
 import de.teamlapen.vampirism.entity.player.vampire.VampirePlayer;
+import net.minecraft.core.Registry;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.LevelAccessor;
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 
-@SuppressWarnings({"unused", "AddedMixinMembersNamePattern"})
+@SuppressWarnings({"unused", "AddedMixinMembersNamePattern", "deprecation"})
 @Mixin(VampirePlayer.class)
 public abstract class VampirePlayerMixin extends FactionBasePlayer<IVampirePlayer> {
     @Shadow(remap = false)
@@ -32,7 +32,6 @@ public abstract class VampirePlayerMixin extends FactionBasePlayer<IVampirePlaye
 
     @Inject(method = "isGettingSundamage", at = @At(value = "TAIL"), remap = false)
     public void noDamageIfInCoveredArmor(LevelAccessor iWorld, boolean forcerefresh, CallbackInfoReturnable<Boolean> cir) {
-        //VampirismCoveredArmor.LOGGER.info("Fuck you the player is in sun"); // I had to do some checking if mixin injection works, ok?
         if (forcerefresh) {
             this.sundamage_cache = this.sundamage_cache && !isInCoveredArmor(player);
         }
@@ -41,8 +40,7 @@ public abstract class VampirePlayerMixin extends FactionBasePlayer<IVampirePlaye
     @Unique
     public boolean isInCoveredArmor(Player player) {
         for (ItemStack stack : player.getArmorSlots()) {
-            String itemRegistryName = stack.getItem().getCreatorModId(stack)+":"+stack.getItem().toString();
-            //VampirismCoveredArmor.LOGGER.info("Fuck you: " + itemRegistryName); // I will just leave this here for someone to find :P
+            String itemRegistryName = Registry.ITEM.getKey(stack.getItem()).toString();
             if (VCACommonConfigs.SUNPROTECTED_ARMOR.get().contains(itemRegistryName) == VCACommonConfigs.INVERT.get()) return false;
         }
         return true;
