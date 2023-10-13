03d7559aa075e53a250105de02dc04760f8771ba
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-10-13 19:36:43 +0200

    Now items display if they are protected

--- src/main/java/com/kermitemperor/vampirismcoveredarmor/VampirismCoveredArmor.java
+++ src/main/java/com/kermitemperor/vampirismcoveredarmor/VampirismCoveredArmor.java
@@ -2,15 +2,23 @@ package com.kermitemperor.vampirismcoveredarmor;
 
 import com.kermitemperor.vampirismcoveredarmor.config.VCACommonConfigs;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.Objects;
 
 
 @Mod(VampirismCoveredArmor.MOD_ID)
@@ -31,10 +39,17 @@ public class VampirismCoveredArmor {
 
     private void setup(final FMLCommonSetupEvent event) {
         // Some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("[VampirismCoveredArmor]: Pre-init phase started");
     }
 
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public void addCoveredArmorTooltip(final ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        if (VCACommonConfigs.SUNPROTECTED_ARMOR.get().contains(Objects.requireNonNull(stack.getItem().getRegistryName()).toString())) {
+            event.getToolTip().add(new TranslatableComponent("tooltip.vampirismcoveredarmor.sunprotected").withStyle(ChatFormatting.DARK_GRAY).withStyle(ChatFormatting.UNDERLINE));
+        }
+    }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
